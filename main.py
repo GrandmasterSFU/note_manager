f66845a67ee25f720bcac54e467566c5171a2b59
GrandmasterSFU <154600609+GrandmasterSFU@users.noreply.github.com>
2025-01-19 21:56:25 +0300

    19/01

---
+++ main.py
@@ -0,0 +1,11 @@
+# '1st program'
+print(9 ** 0.5 * 5)
+# '2nd program'
+print(9.99 > 9.98 and 1000 != 1001)
+# '3rd program'
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+# '4th program'
+str_ = '123.456'
+print(int(float(str_) * 10 % 1230))
