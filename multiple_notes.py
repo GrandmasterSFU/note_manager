0dbf84ef7247282012b57f34be9983e1cd8ec807
grandmastersfu_on_mac <zhenya11-1999@mail.ru>
2025-01-26 23:37:02 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	multiple_notes.py

--- multiple_notes.py
+++ multiple_notes.py
@@ -1,4 +1,5 @@
 print('Добро пожаловать в менеджер заметок! Вы можете добавить новую заметку')
+note_list = []
 note ={
         'title': None,
         'content': None,
@@ -6,8 +7,19 @@ note ={
 while True:
     tittle = input('Введите название заметки')
     if tittle == 'YES':
+    qa = input('Хотите ли вы создать новую заметку? ')
+    if qa == 'YES':
+        username = input('Введите имя пользователя: ')
+        tittle = input('Введите заголовок заметки: ')
+        content = input('Введите описание заметки: ')
+        note['title'] = tittle
+        note['content'] = content
+        print('Заметка успешно создана!')
+    note_list.append(note)
+    if qa == 'NO':
         break
     content = input('Введите содержание заметки')
     status = input('Введите статус заметки')
 
     note.update()
+print(note)
