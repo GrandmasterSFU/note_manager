dcebea5955141303edfbda47b35adbfa7688fac7
grandmastersfu_on_mac <zhenya11-1999@mail.ru>
2025-01-27 00:06:11 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	multiple_notes.py

--- delete_note.py
+++ delete_note.py
@@ -1,22 +1,48 @@
 notes_list = [
     {
+        'username': 'User 1',
         'title': 'Заголовок 1',
         'content': 'Описание заметки 1',
         'status': 'в процессе',
-        'created_date': '01-01-2022',
-        'deadline': '03-01-2022'
     },
     {
+        'username': 'User 2',
         'title': 'Заголовок 2',
         'content': 'Описание заметки 2',
-        'status': 'закрыта',
-        'created_date': '02-01-2022',
-        'deadline': '04-01-2022'
+        'status': 'завершена',
     },
     {
+        'username': 'User 3',
         'title': 'Заголовок 3',
         'content': 'Описание заметки 3',
-        'status': 'открыта',
-        'created_date': '03-01-2022',
+        'status': 'в процессе',
     }
 ]
+
+while True:
+    intr = input('Хотите ли вы удалить заметки?')
+    if intr == 'да':
+        query = input('Укажите критерий для удаления: Заголовок или Имя пользователя ')
+        if query == 'Заголовок':
+            title_to_delete = input('Укажите заголовок заметки для удаления: ')
+            for note in notes_list:
+                if note['title'] == title_to_delete:
+                    notes_list.remove(note)
+                    print('Заметка успешно удалена.')
+                    break
+            else:
+                print('Заметка не найдена.')
+
+        if query == 'Имя пользователя':
+            title_to_delete = input('Укажите имя пользователя заметки для удаления: ')
+            for note in notes_list:
+                if note['username'] == title_to_delete:
+                    notes_list.remove(note)
+                    print('Заметка успешно удалена.')
+                    break
+            else:
+                print('Заметка не найдена.')
+        else:
+            print('Критерий не определен')
+    if intr == 'нет': break
+print(notes_list)
